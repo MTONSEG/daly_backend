5fae1f96491e980891d3e9077556551a53ddeab4
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-02-12 17:22:43 +0200

    add content types

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ImagesProducts extends Schema.Component {
+  collectionName: 'components_images_products';
+  info: {
+    displayName: 'Products';
+    icon: 'picture';
+  };
+  attributes: {
+    url: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'images.products': ImagesProducts;
+    }
+  }
 }
