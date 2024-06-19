e270aabcd73aed07c17226fb08195c8d639b20e5
Maksym Korostelov <flying1519@gmail.com>
2024-06-19 20:52:01 +0300

    change to production

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ if (process.env.NODE_ENV === "production") {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        host: env("DATABASE_HOST", 'ep-small-grass-a2gcqwg0.eu-central-1.aws.neon.tech'),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", 'shob_db'),
+        user: env("DATABASE_USERNAME", 'mtonsiq'),
+        password: env("DATABASE_PASSWORD", 'BL4EVY7hWTdI'),
         schema: env("DATABASE_SCHEMA"),
         ssl: {
           rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
@@ -21,11 +21,11 @@ if (process.env.NODE_ENV === "production") {
     connection: {
       client: "postgres",
       connection: {
-        host: env('DATABASE_HOST', 'ep-small-grass-a2gcqwg0.eu-central-1.aws.neon.tech'),
-        port: env.int("LOCAL_DATABASE_PORT", 5432),
-        database: env("LOCAL_DATABASE_NAME", 'shob_db'),
-        user: env("LOCAL_DATABASE_USERNAME", 'mtonsiq'),
-        password: env("LOCAL_DATABASE_PASSWORD", 'BL4EVY7hWTdI'),
+        host: env('DATABASE_HOST'),
+        port: env.int("LOCAL_DATABASE_PORT"),
+        database: env("LOCAL_DATABASE_NAME"),
+        user: env("LOCAL_DATABASE_USERNAME"),
+        password: env("LOCAL_DATABASE_PASSWORD"),
         schema: env("LOCAL_DATABASE_SCHEMA"),
         ssl: {
           rejectUnauthorized: env.bool("LOCAL_DATABASE_SSL_SELF", false),
